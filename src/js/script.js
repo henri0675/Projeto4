111d5cfc293aac6fe362e13916bf5ef63af17806
Henrique <henriinfanti@gmail.com>
2025-05-05 08:56:11 -0300

    Criando o slideShow e estilizando o site

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+//DECLARANDO UM ARRAY DE IMAGENS
+
+let imagens =[
+'src/assets/imagem3.jpg',
+'src/assets/imagem1.jpg',
+'src/assets/imagem2.jpg',
+]
+
+//DECLARANDO AS VARIAVEIS
+
+let i = 0;
+let tempo = 5000;
+
+//CRIANDO A FUNÇÃO DO SLIDESHOW
+
+function slideShow(){
+    document.getElementById('image').src = imagens[i];
+    i++;
+    if(i == imagens.length){
+        i=0;
+    }
+    setTimeout("slideShow()",tempo)
+}
+slideShow();
